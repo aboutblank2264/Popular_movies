b2258e622af7d3100e1b45be747c591512fd37ff
Justin Yang <aboutblank2264@gmail.com>
2018-06-01 00:36:38 +0800

    adding some basic caching

--- app/src/main/java/com/aboutblank/popular_movies/data/DataRepository.java
+++ app/src/main/java/com/aboutblank/popular_movies/data/DataRepository.java
@@ -1,6 +1,7 @@
 package com.aboutblank.popular_movies.data;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.aboutblank.popular_movies.data.domain.MovieDbRequest;
 import com.aboutblank.popular_movies.data.local.LocalDataSource;
@@ -24,6 +25,14 @@ public class DataRepository implements DataSource {
 
     private static DataRepository instance;
 
+    private List<Movie> highestRankingMoviesCache;
+    private List<Movie> popularMoviesCache;
+    private List<Movie> favoriteMoviesCache;
+
+    private String latestMovieId;
+    private List<MovieReview> latestMovieReviews;
+    private List<MovieVideo> latestMovieVideos;
+
     private DataRepository(DatabaseReader databaseReader) {
         this.remoteDataSource = RemoteDataSourceImpl.getInstance();
         this.localDataSource = LocalDataSourceImpl.getInstance(databaseReader);
@@ -39,158 +48,223 @@ public class DataRepository implements DataSource {
 
     @Override
     public void getHighestRatedMovies(@NonNull final LoadListOfDataCallBack<Movie> callBack) {
-        remoteDataSource.getHighestRatedMovies(new LoadListOfDataCallBack<Movie>() {
-            @Override
-            public MovieDbRequest getRequest() {
-                if (callBack.getRequest() != null) {
-                    return callBack.getRequest();
-                } else {
-                    return new MovieDbRequest();
+        if (highestRankingMoviesCache != null) {
+            Log.d("Highest Ranking Movies", "Cached!");
+
+            callBack.onDataLoaded(highestRankingMoviesCache);
+        } else {
+            remoteDataSource.getHighestRatedMovies(new LoadListOfDataCallBack<Movie>() {
+                @Override
+                public MovieDbRequest getRequest() {
+                    if (callBack.getRequest() != null) {
+                        return callBack.getRequest();
+                    } else {
+                        return new MovieDbRequest();
+                    }
                 }
-            }
 
-            @Override
-            public DataType getDataType() {
-                return DataType.HIGHEST_RATED;
-            }
+                @Override
+                public DataType getDataType() {
+                    return DataType.HIGHEST_RATED;
+                }
 
-            @Override
-            public void onDataLoaded(List<Movie> movies) {
-                callBack.onDataLoaded(movies);
-            }
+                @Override
+                public void onDataLoaded(List<Movie> movies) {
+                    highestRankingMoviesCache = movies;
 
-            @Override
-            public void onDataNotAvailable(String error) {
-                callBack.onDataNotAvailable(error);
-            }
-        });
+                    callBack.onDataLoaded(movies);
+                }
+
+                @Override
+                public void onDataNotAvailable(String error) {
+                    callBack.onDataNotAvailable(error);
+                }
+            });
+        }
     }
 
     @Override
     public void getPopularMovies(@NonNull final LoadListOfDataCallBack<Movie> callBack) {
-        remoteDataSource.getPopularMovies(new LoadListOfDataCallBack<Movie>() {
-            @Override
-            public MovieDbRequest getRequest() {
-                if (callBack.getRequest() != null) {
-                    return callBack.getRequest();
-                } else {
-                    return new MovieDbRequest();
+        if (popularMoviesCache != null) {
+            Log.d("Popular Movies", "Cached!");
+
+            callBack.onDataLoaded(popularMoviesCache);
+        } else {
+            remoteDataSource.getPopularMovies(new LoadListOfDataCallBack<Movie>() {
+                @Override
+                public MovieDbRequest getRequest() {
+                    if (callBack.getRequest() != null) {
+                        return callBack.getRequest();
+                    } else {
+                        return new MovieDbRequest();
+                    }
                 }
-            }
 
-            @Override
-            public DataType getDataType() {
-                return DataType.POPULAR;
-            }
+                @Override
+                public DataType getDataType() {
+                    return DataType.POPULAR;
+                }
 
-            @Override
-            public void onDataLoaded(List<Movie> movies) {
-                callBack.onDataLoaded(movies);
-            }
+                @Override
+                public void onDataLoaded(List<Movie> movies) {
+                    popularMoviesCache = movies;
 
-            @Override
-            public void onDataNotAvailable(String error) {
-                callBack.onDataNotAvailable(error);
-            }
-        });
+                    callBack.onDataLoaded(movies);
+                }
+
+                @Override
+                public void onDataNotAvailable(String error) {
+                    callBack.onDataNotAvailable(error);
+                }
+            });
+        }
     }
 
     @Override
     public void getFavoritedMovies(@NonNull final GetDataForFavoritedMoviesCallBack callBack) {
-        localDataSource.getFavoritedMovies(new LocalDataSource.GetFavoritedMoviesCallBack() {
+        if (favoriteMoviesCache != null) {
+            Log.d("Favorite Movies", "Cached!");
+
+            callBack.onDataLoaded(favoriteMoviesCache);
+        } else {
+            localDataSource.getFavoritedMovies(new LocalDataSource.GetFavoritedMoviesCallBack() {
+                @Override
+                public void onDataLoaded(List<Integer> movieIds) {
+                    callBack.setMovieIds(movieIds);
+
+                    remoteDataSource.getFavoritedMovies(new GetDataForFavoritedMoviesCallBack() {
+                        @Override
+                        public List<Integer> getMovieIds() {
+                            return callBack.getMovieIds();
+                        }
+
+                        @Override
+                        public void setMovieIds(List<Integer> movieIds) {
+                            //Do nothing.
+                        }
+
+                        @Override
+                        public void onDataLoaded(List<Movie> movies) {
+                            favoriteMoviesCache = movies;
+
+                            callBack.onDataLoaded(movies);
+                        }
+
+                        @Override
+                        public void onDataNotAvailable(String error) {
+                            callBack.onDataNotAvailable(error);
+                        }
+                    });
+                }
+
+                @Override
+                public void onDataNotAvailable(String error) {
+                    callBack.onDataNotAvailable(error);
+                }
+            });
+        }
+    }
+
+    //Not used
+    @Override
+    public void getMovie(@NonNull final LoadMovieCallBack callBack) {
+        localDataSource.getMovie(new LoadMovieCallBack() {
             @Override
-            public void onDataLoaded(List<Integer> movieIds) {
-                callBack.setMovieIds(movieIds);
-                remoteDataSource.getFavoritedMovies(callBack);
+            public int getMovieId() {
+                return callBack.getMovieId();
+            }
+
+            @Override
+            public void onDataLoaded(Movie movieItem) {
+                callBack.onDataLoaded(movieItem);
             }
 
             @Override
             public void onDataNotAvailable(String error) {
                 callBack.onDataNotAvailable(error);
-            }
-        });
-    }
-
-    @Override
-    public void getMovie(@NonNull final LoadMovieCallBack callback) {
-        localDataSource.getMovie(new LoadMovieCallBack() {
-            @Override
-            public int getMovieId() {
-                return callback.getMovieId();
-            }
-
-            @Override
-            public void onDataLoaded(Movie movieItem) {
-                callback.onDataLoaded(movieItem);
-            }
-
-            @Override
-            public void onDataNotAvailable(String error) {
-                callback.onDataNotAvailable(error);
             }
         });
     }
 
     @Override
     public void getMovieReviews(@NonNull final LoadListOfDataCallBack<MovieReview> callBack) {
-        remoteDataSource.getMovieReviews(new LoadListOfDataCallBack<MovieReview>() {
-            @Override
-            public MovieDbRequest getRequest() {
-                if (callBack.getRequest() != null) {
-                    return callBack.getRequest();
-                } else {
-                    return new MovieDbRequest();
+        if (latestMovieReviews != null && latestMovieId.equals(callBack.getRequest().getId())) {
+            Log.d("Movie Reviews", "Cached!");
+
+            callBack.onDataLoaded(latestMovieReviews);
+        } else {
+            remoteDataSource.getMovieReviews(new LoadListOfDataCallBack<MovieReview>() {
+                @Override
+                public MovieDbRequest getRequest() {
+                    if (callBack.getRequest() != null) {
+                        return callBack.getRequest();
+                    } else {
+                        return new MovieDbRequest();
+                    }
                 }
-            }
 
-            @Override
-            public DataType getDataType() {
-                return callBack.getDataType();
-            }
+                @Override
+                public DataType getDataType() {
+                    return callBack.getDataType();
+                }
 
-            @Override
-            public void onDataLoaded(List<MovieReview> reviews) {
-                callBack.onDataLoaded(reviews);
-            }
+                @Override
+                public void onDataLoaded(List<MovieReview> reviews) {
+                    latestMovieReviews = reviews;
+                    latestMovieId = callBack.getRequest().getId();
 
-            @Override
-            public void onDataNotAvailable(String error) {
-                callBack.onDataNotAvailable(error);
-            }
-        });
+                    callBack.onDataLoaded(reviews);
+                }
+
+                @Override
+                public void onDataNotAvailable(String error) {
+                    callBack.onDataNotAvailable(error);
+                }
+            });
+        }
     }
 
     @Override
     public void getMovieVideos(@NonNull final LoadListOfDataCallBack<MovieVideo> callBack) {
-        remoteDataSource.getMovieVideos(new LoadListOfDataCallBack<MovieVideo>() {
-            @Override
-            public MovieDbRequest getRequest() {
-                if (callBack.getRequest() != null) {
-                    return callBack.getRequest();
-                } else {
-                    return new MovieDbRequest();
+        if (latestMovieVideos != null && latestMovieId.equals(callBack.getRequest().getId())) {
+            Log.d("Movie Videos", "Cached!");
+
+            callBack.onDataLoaded(latestMovieVideos);
+        } else {
+            remoteDataSource.getMovieVideos(new LoadListOfDataCallBack<MovieVideo>() {
+                @Override
+                public MovieDbRequest getRequest() {
+                    if (callBack.getRequest() != null) {
+                        return callBack.getRequest();
+                    } else {
+                        return new MovieDbRequest();
+                    }
                 }
-            }
 
-            @Override
-            public DataType getDataType() {
-                return callBack.getDataType();
-            }
+                @Override
+                public DataType getDataType() {
+                    return callBack.getDataType();
+                }
 
-            @Override
-            public void onDataLoaded(List<MovieVideo> videos) {
-                callBack.onDataLoaded(videos);
-            }
+                @Override
+                public void onDataLoaded(List<MovieVideo> videos) {
+                    latestMovieVideos = videos;
+                    latestMovieId = callBack.getRequest().getId();
 
-            @Override
-            public void onDataNotAvailable(String error) {
-                callBack.onDataNotAvailable(error);
-            }
-        });
+                    callBack.onDataLoaded(videos);
+                }
+
+                @Override
+                public void onDataNotAvailable(String error) {
+                    callBack.onDataNotAvailable(error);
+                }
+            });
+        }
     }
 
     @Override
     public void addMovieToFavorite(@NonNull AddRemoveMovieFavoritesCallBack callBack) {
+        favoriteMoviesCache = null;
         localDataSource.addMovieToFavorite(callBack);
     }
 
